7a528fdfc598b1fb1b5af83d1b5aac7949bcb224
Tarun Jhamb <tarunjhamb2705@gamil.com>
2024-07-27 19:00:39 +0530

    Basic structure of app, routes done

---
+++ src/layouts/rootLayout/RootLayout.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./rootLayout.css";
+import { Link, Outlet } from 'react-router-dom';
+import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+
+
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+
+const RootLayout = () => {
+  return (
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <div className='rootLayout'>
+          <header>
+              <Link to="/" className='logo'>
+                  <img src='/logo.png'/>
+                  <span>AI CHAT</span>
+              </Link>
+              <div className="user">
+                {/* <SignedOut>
+                  <SignInButton />
+                </SignedOut> */}
+                <SignedIn>
+                  <UserButton />
+                </SignedIn>
+              </div>
+          </header>
+          <main>
+              <Outlet/>
+          </main>
+      </div>
+    </ClerkProvider>
+  )
+}
+
+export default RootLayout
---
+++ src/routes/signInPage/SignInPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./signInPage.css"
+import { SignIn } from '@clerk/clerk-react'
+
+const SignInPage = () => {
+  return (
+    <div className='signInPage'>
+      <SignIn path="/sign-in" />
+    </div>
+  )
+}
+
+export default SignInPage
